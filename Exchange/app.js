b06db528b9aaa04f495c10c9344ea8408ef11290
GustavoCostaAM <gustavo.costa.m949@gmail.com>
2022-11-19 14:30:41 -0300

    Back end modules

--- Exchange/app.js
+++ Exchange/app.js
@@ -1,7 +1,2 @@
-var app = require('http').createServer(9494)
-app.listen(9494);
-console.log(`O projeto esta em execução com sucesso!`)
-function resposta (req, res) {
-    res.writeHead(200);
-    res.end("Ola, o servidor esta funcionando corretamente.");
-}
+var io = require("socket.io");
+var express = require("express");
